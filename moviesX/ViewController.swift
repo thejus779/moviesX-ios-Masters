38dfb554134b283d16627fd063bdeaf53f3efb86
thejus manoharan <shine@thejuss-macbook-pro.local>
2018-04-22 14:42:31 +0200

    Initial Commit

---
+++ moviesX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  moviesX
+//
+//  Created by thejus manoharan on 22/04/2018.
+//  Copyright © 2018 thejus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
